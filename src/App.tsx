79bd8bc4c21012e8b3e684ebbe821b3f421bc542
SamyakNagdive <154076105+SamyakNagdive@users.noreply.github.com>
2024-11-18 13:54:44 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AddressForm from './components/AddressForm';
+
+function App() {
+  return <AddressForm />;
+}
+
+export default App;
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface Address {
+  doctorName: string;
+  hospitalClinic: string;
+  flatNo: string;
+  society: string;
+  area: string;
+  city: string;
+  pincode: string;
+  mobile: string;
+}
